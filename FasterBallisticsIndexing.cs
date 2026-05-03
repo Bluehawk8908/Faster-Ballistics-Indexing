bd15ba59f77e01e298c5c142711c0f3787aad69c
Bluehawk2008 <bluehawk2008@gmail.com>
2026-05-02 20:11:12 -0400

    Update FasterBallisticsIndexing.cs
    More direct solution, less code

--- FasterBallisticsIndexing.cs
+++ FasterBallisticsIndexing.cs
@@ -1,9 +1,9 @@
-﻿using GHPC.Weapons;
+using GHPC.Weapons;
 using MelonLoader;
 using HarmonyLib;
 using FasterBallisticsIndexing;
 
-[assembly: MelonInfo(typeof(FasterBallistics), "Faster Ballistics Indexing", "1.0.0", "Bluehawk")]
+[assembly: MelonInfo(typeof(FasterBallistics), "Faster Ballistics Indexing", "1.0.1", "Bluehawk")]
 [assembly: MelonGame("Radian Simulations LLC", "GHPC")]
 
 namespace FasterBallisticsIndexing
@@ -12,13 +12,12 @@ namespace FasterBallisticsIndexing
     {
         public override void OnInitializeMelon()
         {
-            MelonLogger.Msg("Patching reload functions");
-            
+            MelonLogger.Msg("Patching reload function");            
         }
     }
 
-    [HarmonyPatch(typeof(GHPC.Weapons.AmmoFeed), "Reload")]
-    public static class ReloadOnR
+    [HarmonyPatch(typeof(GHPC.Weapons.AmmoFeed), "FeedNewClip")]
+    public static class Reload
     {
         private static void Prefix(GHPC.Weapons.AmmoFeed __instance)
         {
@@ -27,17 +26,5 @@ namespace FasterBallisticsIndexing
             FireControlSystem fcs = wep.FCS;
             if (queued != null) { fcs.CurrentAmmoType = queued; }           
         }
-    }
-
-    [HarmonyPatch(typeof(GHPC.Weapons.AmmoFeed), "SetNextClipType")]
-    public static class ReloadOnNum
-    {
-        private static void Postfix(GHPC.Weapons.AmmoFeed __instance)
-        {
-            AmmoType queued = __instance.QueuedClipType.MinimalPattern[0].AmmoType;
-            WeaponSystem wep = __instance.gameObject.GetComponent<WeaponSystem>();
-            FireControlSystem fcs = wep.FCS;
-            if (queued != null && __instance.AmmoTypeInBreech == null) { fcs.CurrentAmmoType = queued; }
-        }
-    }   
+    }    
 }
